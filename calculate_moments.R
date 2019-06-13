9715f60efd45395cfdacdf844c8ba01e4fe3418c
Petey <pdotjpr@gmail.com>
2019-06-13 15:37:45 -0400

    abstracted functinality + 3 type hierarchy

---
+++ calculate_moments.R
@@ -0,0 +1,59 @@
+
+#Computes the first and second moment of the population vector at time Tf for a 
+#branching process with parameters E, P, R and initial population 
+calculate_moments = function(E,P,R,Z0,Tf){
+  library(expm)
+  library(deSolve)
+  
+  d = length(Z0)
+  R_prime = matrix(rep(0,nrow(E)*d), c(nrow(E),d))
+  R_prime[cbind(1:nrow(E), P)] = R
+  
+  lamb = colSums(R_prime)
+  b = t(t(E)%*%R_prime)/lamb
+  
+  #Diff EQ: M(t) = exp(At)
+  A =  lamb*(b - diag(d))
+  M = expm(A*Tf)
+  
+  C = array(rep(0,d**3), c(d, d, d)); #matrix of second derivatives of offspring PGF
+  
+  for(i in 1:d){
+    i_mat = t(t(E*E[,i])%*%R_prime)/lamb
+    i_mat[,i] = i_mat[,i] - b[,i]
+    C[,i,] = t(i_mat)
+    C[i,,] = t(i_mat)
+  }
+  
+  second_moment_de = function(t, state, params){
+    mt = expm(A*t)
+    Beta = matrix(rep(0,d**3), nrow = d, ncol = d*d); #Beta array for second moment ODE
+    for(i in 1:d){
+      ai = lamb[i]
+      Beta[i,] = c(ai*t(mt)%*%C[,,i]%*%mt) #vectorized computation of Beta
+    }
+    x = matrix(state,c(d,d*d))
+    x_prime = c(A%*%x + Beta)
+    return(list(x_prime))
+  }
+  
+  init_state = array(rep(0,d**3),c(d,d,d)) #inital values of second moments
+  for(i in 1:d){
+    init_state[i,i,i] = 1;
+  }
+  init_state = c(init_state)
+  times = seq(0,Tf)
+  
+  out = ode(y = init_state, times, func = second_moment_de, parms = 0)
+  dt = array(out[nrow(out),-1],c(d,d*d)) #second moment array
+  
+  Mu = t(M)%*%Z0 #final mean population matrix
+  Sigma = matrix(Z0%*%dt,c(d,d*d)) #final population covariance matrix
+  
+  for(i in 1:d){
+    for(j in 1:d){
+      Sigma[i,j] =  Sigma[i,j] - Z0%*%(M[,i]*M[,j])
+    }
+  }
+  return(list("mu" = Mu, "sigma" = Sigma))
+}
--- multitype_birth_death.R
+++ multitype_birth_death.R
@@ -11,7 +11,7 @@ bp = function(E, R, P, Z0, times){#Z0 = inital population vector, times = vector
   tf = times[length(times)]
   d = length(Z0)
   nEvents = nrow(E)
-  Zt <- matrix(0, ncol = 2, nrow = length(times)) #store populations at different times
+  Zt <- matrix(0, ncol = d, nrow = length(times)) #store populations at different times
   t = 0
   Zt[1,] = Z0
   Z = Z0
@@ -21,7 +21,7 @@ bp = function(E, R, P, Z0, times){#Z0 = inital population vector, times = vector
   R_prime[cbind(1:nEvents, P)] = R
 
   while(t < tf){
-    eventRates = R_prime%*%Z
+    eventRates = R_prime%*%Z #multiply rates by pop sizes
     lambda = sum(eventRates) #combined rate at which stuff is happening
     if(lambda == 0){ #extinction
       out = list(pop = pop,time = times)
@@ -60,97 +60,6 @@ bpsims <- function(E, R, P, Z0, times, reps)
   }
   return(Z)
 }
-
-
-library(expm)
-d=2
-E =  rbind(c(2, 0),c(1,1),c(1,1), c(0,2), c(0,0),c(0,0))
-P = c(1, 1, 2, 2, 1, 2)
-R = c(.2, .1, .05, .2, .25, .2)
-Z0 = c(1000,500) # initial population vector
-Tf = 5 #final simulation timepoint
-
-R_prime = matrix(rep(0,nrow(E)*d), c(nrow(E),d))
-R_prime[cbind(1:nrow(E), P)] = R
-
-lamb = colSums(R_prime)
-b = t(t(E)%*%R_prime)/lamb
-
-#Diff EQ: M(t) = exp(At)
-A =  lamb*(b - diag(d))
-M = expm(A*Tf)
-
-C = array(rep(0,d**3), c(d, d, d)); #matrix of second derivatives of offspring PGF
-
-for(i in 1:d){
-  i_mat = t(t(E*E[,i])%*%R_prime)/lamb
-  i_mat[,i] = i_mat[,i] - b[,i]
-  C[,i,] = t(i_mat)
-  C[i,,] = t(i_mat)
-}
-
-library(deSolve)
-
-second_moment_de = function(t, state, parameters){
-  mt = expm(A*t)
-  Beta = matrix(rep(0,d**3), nrow = d, ncol = d*d); #Beta array for second moment ODE
-  for(i in 1:d){
-    ai = lamb[i]
-    Beta[i,] = c(ai*t(mt)%*%C[,,i]%*%mt) #vectorized computation of Beta
-  }
-  x = matrix(state,c(d,d*d))
-  x_prime = c(A%*%x + Beta)
-  print(x_prime)
-  return(list(x_prime))
-}
-
-init_state = c(1,0,0,0,0,0,0,1) #inital values of second moments
-times = seq(0,5)
-
-out = ode(y = init_state, times, func = second_moment_de, parms = 0)
-dt = array(out[nrow(out),-1],c(d,d*d)) #second moment array
-
-Mu = t(M)%*%Z0 #final mean population matrix
-Sigma = matrix(Z0%*%dt,c(d,d*d)) #final population covariance matrix
-
-for(i in 1:d){
-  for(j in 1:d){
-    Sigma[i,j] =  Sigma[i,j] - Z0%*%(M[,i]*M[,j])
-  }
-}
-
-X = bpsims(E, R, P, Z0,times = times, reps = 50)
-X <- X %>% group_by(rep)
-names(X)[3:4] <- c("t1_cells", "t2_cells")
-X <- X %>% group_by(rep) %>% mutate(t1_cells_prev = lag(t1_cells), t2_cells_prev = lag(t2_cells),
-                                        t1_cells_0 = first(t1_cells), t2_cells_0 = first(t2_cells),
-                                        dtimes = times - lag(times))
-
-# Remove NA values
-X <- X %>% filter(!is.na(t1_cells_prev))
-
-pop_vec = cbind(X$t1_cells,X$t2_cells)
-init_pop = cbind(X$t1_cells_prev,X$t2_cells_prev)
-model_dtimes = sort(unique(times[-1] - times[-length(times)]))
-
-
-#mean(X[X$times == 1,]$t1_cells)
-#mean(X[X$times == 1,]$t2_cells)
-#var(X[X$times == 1,]$t1_cells)
-#var(X[X$times == 1,]$t2_cells)
-#cov(X[X$times == 1,]$t1_cells,X[X$times == 1,]$t2_cells)
-
-library(rstan)
-options(mc.cores = parallel::detectCores())
-stan_dat <- list(d = d, m = nrow(E), n = nrow(pop_vec), l=1,  pop_vec = pop_vec, init_pop = init_pop, E = E, P = P, times = as.array(model_dtimes), timesIdx = as.numeric(factor(X$dtimes)), Pri_mu = rep(0, nrow(E)), Pri_sig = rep(.25, nrow(E)))
-fit <- stan_model(file = "multitype_birth_death.stan")
-fit.data <- sampling(fit, data = stan_dat, control = list(adapt_delta = 0.8), chains = 4, refresh = 1)
-s = extract(fit.data)
-
-t = format(Sys.time(), "%a_%b_%d_%H:%M:%S_%Y")
-saveRDS(fit.data, paste("saves/",t, "_",toString(d), "type_samples.rds", sep=""))
-saveRDS(R, paste("saves/",t, "_", toString(d), "type_truth.rds", sep=""))
-saveRDS(stan_dat, paste("saves/",t, "_", toString(d), "type_data.rds", sep=""))
 
 
 
