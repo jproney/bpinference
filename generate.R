c1fd0508e4dd934ba5146f9e6afa343715cef458
Petey <pdotjpr@gmail.com>
2019-06-19 10:18:04 -0400

    overhauling parameters to be totally generic

--- generate.R
+++ generate.R
@@ -1,7 +1,7 @@
 #Auto-generates a Stan file for inferring a model where rates have specified functional dependencies
 library(stringr)
 
-#functional_deps is an array of strings encoding functions of  variable 'x1, x2, ..., x9' and constant parameters 'c1','c2',... 'c9'
+#functional_deps is an array of strings encoding functions of  variable 'x1, x2, ...' and constant parameters 'c1','c2',...
 #priors is list of with prior objects for all parameters in order of the rate they contribute to
 #example prior object: p = list(name="gamma",params=c(1,1))
 #filename is name of generated Stan file
@@ -14,17 +14,17 @@ generate = function(functional_deps, priors, filename){
     # convert the expression
     exprn = functional_deps[[i]]
     stanstr = exp_to_stan(exprn)
-    funcs[i] = sprintf("\t\t\tif(func_type[k] == %d){\n\t\t\t\tR_prime[k, P[k]] = %s; \n\t\t\t}\n",i,stanstr) 
+    funcs[i] = sprintf("\t\t\tif(func_type[k] == %d){\n\t\t\t\tR[k, P[k]] = %s; \n\t\t\t}\n",i,stanstr) 
   }
   dists = readLines("allowed_distributions.txt") #load distributions file
   nPri = length(priors)
   priorStrs = rep(0, nPri)
   for(p in 1:nPri){
     ind = which(!is.na(str_extract(dists, paste("^",priors[[p]]$name, " ",sep=""))))
-    if(ind){
+    if(length(ind) > 0){
       nparm = strtoi(str_extract(dists[ind], "[1-9]+"))
       if(nparm != length(priors[[p]]$params)){
-        stop("Incorrect number of parameters")
+        stop("Incorrect number of parameters for prior distribution")
       }
       constraints = str_extract(dists[ind], "[_+]+")
       for(i in 1:nparm){
@@ -36,7 +36,7 @@ generate = function(functional_deps, priors, filename){
     else{
       stop("Invalid prior name!")
     }
-    priorStrs[p] = sprintf("\tR[%d] ~ %s(%s)\n", p, priors[[p]]$name, paste(priors[[p]]$params, collapse=","))
+    priorStrs[p] = sprintf("\tTheta[%d] ~ %s(%s);\n", p, priors[[p]]$name, paste(priors[[p]]$params, collapse=","))
   }
   write(sprintf(template, paste(funcs, collapse=""),  paste(priorStrs,collapse="")), filename)
 }
@@ -60,14 +60,13 @@ exp_to_stan = function(exprn){
     return(exp_to_stan(exprn[[2]]))
   }
   if(!is.na(str_extract(deparse(first),"^c[1-9]$"))){ # function parameters
-    num = strtoi(substr(str_extract(deparse(first),"^c[1-9]$"),2,2))-1
-    if(num == 0){
-      return(sprintf("R[event_idx[k]]", num))
-    }
-    return(sprintf("R[event_idx[k]+%d]", num))
+    s = str_extract(deparse(first),"^c[1-9]+$")
+    num = strtoi(substr(s,2,nchar(s)))
+    return(sprintf("Theta[%d]", num))
   }
   if(!is.na(str_extract(deparse(first),"^x[1-9]$"))){ # variables
-    num = strtoi(substr(str_extract(deparse(first),"^x[1-9]$"),2,2))
+    s = str_extract(deparse(first),"^x[1-9]+$")
+    num = strtoi(substr(s,2,nchar(s)))
     return(sprintf("function_var[l, %d]", num))
   }
   else{
