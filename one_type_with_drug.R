124b1d18afec5b509c5ad16473c4abedd7cd42ee
Petey <pdotjpr@gmail.com>
2019-06-20 12:33:55 -0400

    one-type is worse?

--- one_type_with_drug.R
+++ one_type_with_drug.R
@@ -4,14 +4,14 @@ P = c(1,1)
 Z0 = c(1000) # initial population vector
 Tf = 5 #final simulation timepoint
 
-f1 = function(x){.15 + .2*exp(-x)}
+f1 = function(x){.35}
 f2 = function(x){.2}
 fs = c(f1,f2)
 
 C = matrix(c(0.0,0.2,0.4,0.6,0.8,1.0),6,1)
 
 times = seq(1,Tf)
-X = bpsims_modeling(E, P, Z0, C, fs, times = times, reps = rep(50,6))
+X = bpsims_modeling(E, P, Z0, C, fs, times = times, reps = rep(100,6))
 X <- X %>% group_by(rep)
 names(X)[4] <- "cells"
 names(X)[5] <- "drug_level"
@@ -23,9 +23,9 @@ X <- X %>% filter(!is.na(cells_prev))
 pop_vec = X$cells
 init_pop = X$cells_prev
 
-f = c(quote(c1 + c2*exp(-x1)), quote(c3))
-prior = rep(list(list(name = "normal", params=c(0,.25), bounds = c(0,1))),3)
-ranges = matrix(c(0,1,0,1,0,1),3,2,byrow = T)
+f = c(quote(c1), quote(c2))
+prior = rep(list(list(name = "normal", params=c(0,.25), bounds = c(0,1))),2)
+ranges = matrix(c(0,1,0,1),2,2,byrow = T)
 
 out = create_stan_data(E = E, P= P, final_pop = matrix(pop_vec,ncol=1), init_pop = matrix(init_pop, ncol=1), times = X$dtimes, priors = prior, C = matrix(X$drug_level, ncol=1), functions = f)
 init = uniform_initialize(ranges, 4)
