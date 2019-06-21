06c922b62bbb66d13f97c6ef1059c2fad5c0574c
Petey <pdotjpr@gmail.com>
2019-06-21 11:44:20 -0400

    successful logisticgit add .

--- one_type_with_drug.R
+++ one_type_with_drug.R
@@ -4,16 +4,17 @@ Z0 = c(1000) # initial population vector
 Tf = 5 #final simulation timepoint
 times = seq(1,Tf)
 
-func_deps = c('c[1] + c[2]*exp(-x[1])','c[3]')
-priors = rep(list(list(name="normal",params=c(0,.25), bounds=c(0,5))),3)
+func_deps = c('c[1] + c[2]/(1 + exp(c[3]*(x[1] - c[4])))','c[5]') #logistic function
+priors = rep(list(list(name="normal",params=c(0,.25), bounds=c(0,5))),5)
+priors[[3]] = list(name="uniform",params=c(5,15), bounds=c(5,15))
 
-mod = bp_model(E, P, func_deps, 3, 1)
+mod = bp_model(E, P, func_deps, 5, 1)
 
-simulation_params = c(0.15, .2, 0.10)
+simulation_params = c(0.15, .2, 10, 0.5, 0.10)
 
-C = matrix(c(0.0,0.1,0.2,0.4,0.8), ncol=1)
+C = matrix(c(0.0,0.2,0.4,0.6,0.8,1.0), ncol=1)
 
-X = bpsims(mod, simulation_params, Z0, times, rep(50,5), C)
+X = bpsims(mod, simulation_params, Z0, times, rep(50,6), C)
 
 dat = stan_data_from_simulation(X, mod)
 
@@ -22,6 +23,7 @@ generate(mod, priors, "test3.stan")
 options(mc.cores = parallel::detectCores())
 
 ranges = matrix(rep(c(0,1), length(simulation_params)),ncol=2,byrow = T)
+ranges[3,] = c(5,15)
 init = uniform_initialize(ranges, 4)
 
 library(rstan)
