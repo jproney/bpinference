3e12f4b46b2eb18810bf61dfbe2f604f4fe42d56
James <jamesproney@gmail.com>
2019-06-25 23:34:51 -0400

    finished first round of benchmarks

--- cluster_scripts/one_type_benchmark.sh
+++ cluster_scripts/one_type_benchmark.sh
@@ -7,11 +7,15 @@ cd $folder_dir
 
 while read -r args
 do
- export args
  # Command to send to nodes - will use sbatch here
- sbatch --job-name=one_type_benchmark \
-    $folder_dir/cluster_scripts/one_type_benchmark.sbatch
-echo "${args}"
-sleep 0.3
+ nsims=$(echo ${args} | grep -o '^[0-9]*') 
+ nreps=$(echo ${args} | grep -o '[0-9]*$')
+ export nsims
+ for ((c=1; c<=$nreps; c++))
+ do
+   sbatch  -o /dev/null --job-name=one_type_benchmark \
+   $folder_dir/cluster_scripts/one_type_benchmark.sbatch 
+   echo "${nsims}"
+ done
 done < $folder_dir/benchmarks/one_type_benchmark_params.txt
 
