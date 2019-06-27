016dfe600303ee11d0cb1d0610f6947cacfcd3c0
Petey <pdotjpr@gmail.com>
2019-06-27 10:03:24 -0400

    logistic benchmark scripts

---
+++ cluster_scripts/logistic_benchmark.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# location for home directory to work with
+folder_dir="/michorlab/jroney/bpinference"
+
+cd $folder_dir
+
+while read -r args
+do
+ # Command to send to nodes - will use sbatch here
+ nsims=$(echo ${args} | grep -o '^[0-9]*') 
+ nreps=$(echo ${args} | grep -o '[0-9]*$')
+ export nsims
+ for ((c=1; c<=$nreps; c++))
+ do
+   sbatch  -o /dev/null --job-name=logistic_benchmark \
+   $folder_dir/cluster_scripts/logstic_benchmark.sbatch 
+   echo "${nsims}"
+ done
+done < $folder_dir/benchmarks/logistic_benchmark_params.txt
+
