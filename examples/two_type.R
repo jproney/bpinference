38f114efa0b6c7ade92f9dee2f877ae5e5aa8604
Petey <pdotjpr@gmail.com>
2019-07-03 23:15:41 -0400

    mathboi unlocked

--- examples/two_type.R
+++ examples/two_type.R
@@ -1,6 +1,6 @@
 e_mat =  rbind(c(2, 0),c(1,1),c(1,1), c(0,2), c(0,0),c(0,0))
 p_vec = c(1, 1, 2, 2, 1, 2)
-z0 = c(1000,500) # initial population vector
+z0 = c(100,50) # initial population vector
 tf = 5 #final simulation timepoint
 times = seq(0,tf)
 
@@ -11,9 +11,20 @@ mod = bp_model(e_mat, p_vec, func_deps, 6, 0)
 
 simulation_params = c(0.20, 0.10, 0.05, 0.20, 0.25, 0.20)
 
-simulation_dat = bpsims(mod, simulation_params, z0, times, 50)
+simulation_dat = bpsims(mod, simulation_params, z0, times, 5000)
 
 dat = stan_data_from_simulation(simulation_dat, mod)
+
+
+reg = solve(t(dat$init_pop)%*%dat$init_pop)%*%t(dat$init_pop)%*%dat$pop_vec
+
+mu_hat = z0%*%reg
+
+sample_var = t(apply(dat$init_pop%*%reg - dat$pop_vec, 1 , function(s){s%*%t(s)}))
+
+mom2 = solve(t(dat$init_pop)%*%dat$init_pop)%*%t(dat$init_pop)%*%sample_var
+
+sig_hat = mom2[1,]*z0[1] + mom2[2,]*z0[2]
 
 generate(mod, priors, "test.stan")
 
