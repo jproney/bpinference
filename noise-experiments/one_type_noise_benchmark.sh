84d2967f2c32b06a5c0954836a2cc9bfa7dbe8f1
James <jamesproney@gmail.com>
2019-07-25 10:11:16 -0400

    fixed cluster scripts for noise benchmark

---
+++ noise-experiments/one_type_noise_benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# location for home directory to work with
+folder_dir="/michorlab/jroney/bpinference"
+
+cd $folder_dir
+
+while read -r args
+do
+ # Command to send to nodes - will use sbatch here
+ noise=$(echo ${args}) 
+ export noise
+ sbatch --job-name=one_type_noise \
+ $folder_dir/noise-experiments/one_type_noise_benchmark.sbatch 
+ echo "${noise}"
+done < $folder_dir/noise-experiments/noise_params.txt
